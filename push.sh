fd95142324fed8d3b3aa05d705c4695550a42359
Beniamin Czaplicki <beniamin.czaplicki@gmail.com>
2025-05-05 13:05:12 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-# Save as push.sh and fill in your token and repo
-
-GITHUB_USERNAME="ludomirc"
-GITHUB_TOKEN="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"
-REPO_NAME="elk-stack"
-
-git add .
-git commit -m "${1:-Auto commit}"
-git push "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${REPO_NAME}.git" main
 
